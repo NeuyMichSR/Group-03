d7f2997f9291b369e81626e349583faf8024399b
NeuyMichSR <56514563+NeuyMichSR@users.noreply.github.com>
2020-01-18 14:21:41 +0700

    update file

--- index.php
+++ index.php
@@ -16,11 +16,31 @@
     // start slide
     require_once('slide.php');
     //end slide
-    
+   
     //start popular contence
     require_once('popular.php');
     // end popular contence
-
+    
+    // start komsan
+    require_once('komsan.php');
+    // end komsan
+  
+    // somros
+    require_once('somros.php');
+    //end somros
+ 
+    // tos eat
+    require_once('tos-eat.php');
+    // end tos eat
+   
+    // sport
+    require_once('sport.php');
+    // end sport
+   
+    // blaek blaek
+    require_once('blaek-blaek.php');
+    // end blaek blaek
+   
     // start footer
     require_once('footer.php');
     // end footer
---
+++ sport.php
@@ -0,0 +1,63 @@
+    <div class="site-wrap" id="home-section">
+        <div class="container container-list-new">
+            <div>
+                <a class="list-a-komsan1" href="#">កីឡា</a>
+                <a class="list-a-komsan2" href="#">អត្ថបទច្រើនទៀត<i class="border fas fa-chevron-right" style="padding: 3px;"></i></a>
+            </div>
+            <div class="col-xl-4 col-lg-7 col-md-6 col-sm-12 list-komsan1">
+                <a class="position-relative img-scale-animate mb-30" href="./images/sport/7.jpg">
+                    <img src="./images/sport/7.jpg" alt="" style="width: 100%;">
+                </a>
+                <i class="far fa-calendar-alt"> <b>2020-01-13</b></i>
+                <h6 class="list-komsan1-h6">
+                    <a class="stretched-link" href="#">ក្រុមបិសាចក្រហម Manchester United កំពុងប្រជែងទិញតារាឆ្នើមម្នាក់របស់ក្រុម Barcelona</a>
+                </h6>
+            </div>
+            <div class="col-xl-8 col-lg-5 col-md-6 col-sm-12 list-komsan2">
+                <div class="row keep-items-4-md">
+                    <div class="col-xl-4 col-lg-6 col-md-6 col-sm-4 col-6">
+                        <a href="./images/sport/2.jpg"><img src="./images/sport/2.jpg" alt="" style="width: 100%;"></a>
+                        <i class="far fa-calendar-alt"> 2020-01-13</i>
+                        <h6 class="list-komsan1-h6">
+                            <a class="stretched-link" href="#">កីឡាករ ៥ រូបដែល Real Madrid អាចនឹងនាំយកមកចូលរួមជាមួយក្លឹបក្នុងរដូវកាលទីផ្សារដោះដូរកីឡាករខែមករានេះ</a>
+                        </h6>
+                    </div>
+                    <div class="col-xl-4 col-lg-6 col-md-6 col-sm-4 col-6">
+                        <a href="./images/sport/3.jpg"><img src="./images/sport/3.jpg" alt="" style="width: 100%;"></a>
+                        <i class="far fa-calendar-alt"> <b>2020-01-13</b></i>
+                        <h6 class="list-komsan1-h6">
+                            <a class="stretched-link" href="#">ដោះដូរកីឡាករ៖ លឺថា Man Utd ចង់​បាន Wolves ដាក់​តម្លៃ​ខ្លួន​ Raul Jimenez ១០០លានផោន</a>
+                        </h6>
+                    </div>
+                    <div class="col-xl-4 col-lg-6 col-md-6 col-sm-4 col-6">
+                        <a href="./images/sport/4.jpg"><img src="./images/sport/4.jpg" alt="" style="width: 100%;"></a>
+                        <i class="far fa-calendar-alt"> <b>2020-01-13</b></i>
+                        <h6 class="list-komsan1-h6">
+                            <a class="stretched-link" href="#">លោក Zidane ឈឺក្បាលខ្ទោកៗក្រោយ​ខ្សែប្រយុទ្ធ​២​រូបអវត្តមានក្នុងពាន ​Spanish Supercopa​ នៅ​អារ៉ាប៊ីសាអូឌីត</a>
+                        </h6>
+                    </div>
+                    <div class="col-xl-4 col-lg-6 col-md-6 col-sm-4 col-6">
+                        <a href="./images/sport/5.jpg"><img src="./images/sport/5.jpg" alt="" style="width: 100%;"></a>
+                        <i class="far fa-calendar-alt"> <b>2020-01-13</b></i>
+                        <h6 class="list-komsan1-h6">
+                            <a class="stretched-link" href="#">អាមេរិកលុបចោល​គម្រោងបោះ​ជុំរំហ្វឹកហាត់នៅ​កាតារដោយ​កំដៅនយោបាយនៅមជ្ឈឹមបូព៌ា</a>
+                        </h6>
+                    </div>
+                    <div class="col-xl-4 col-lg-6 col-md-6 col-sm-4 col-6">
+                        <a href="./images/sport/6.jpg"><img src="./images/sport/6.jpg" alt="" style="width: 100%;"></a>
+                        <i class="far fa-calendar-alt"> <b>2020-01-13</b></i>
+                        <h6 class="list-komsan1-h6">
+                            <a class="stretched-link" href="#">Zlatan៖ ខ្ញុំចាស់​ហើយ ខ្ញុំមិន​អាចញាក់​បាន​ទេ តែ​ខ្ញុំ​អាច​ស៊ុតពី​ចម្ងាយបាន​តើ!</a>
+                        </h6>
+                    </div>
+                    <div class="col-xl-4 col-lg-6 col-md-6 col-sm-4 col-6">
+                        <a href="./images/sport/1.jpg"><img src="./images/sport/1.jpg" alt="" style="width: 100%;"></a>
+                        <i class="far fa-calendar-alt"> <b>2020-01-13</b></i>
+                        <h6 class="list-komsan1-h6">
+                            <a class="stretched-link" href="#">កីឡាករវ័យក្មេងម្នាក់​ដែល​ពិភពលោកមិនសូវស្គាល់ជិត​ផ្ទេរ​ទៅ Real Madrid ហើយ</a>
+                        </h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
