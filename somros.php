88fff5fb5a17494e9b5fb1448702f40fa5cdede0
kevrida2000 <59823572+kevrida2000@users.noreply.github.com>
2020-01-20 13:52:28 +0700

    update rida

--- somros.php
+++ somros.php
@@ -8,7 +8,7 @@
                 <a class="list-a-komsan2" href="#">អត្ថបទច្រើនទៀត<i class="border fas fa-chevron-right" style="padding: 3px;"></i></a>
             </div>
             <div class="col-xl-4 col-lg-7 col-md-6 col-sm-12 list-komsan1">
-                <a class="position-relative img-scale-animate mb-30" href="./images/3.jpg"><img src="./images/3.jpg" alt="" style="width: 100%;"></a> <i class="far fa-calendar-alt"> <b>2020-01-13</b></i>
+                <a class="position-relative img-scale-animate mb-30" href="./images/popular/pic7.jpg"><img src="./images/popular/pic7.jpg" alt="" style="width: 100%;"></a> <i class="far fa-calendar-alt"> <b>2020-01-13</b></i>
                 <h6 class="list-komsan1-h6">
                     <a class="stretched-link" href="#">មានធាងដូង ​ធាងត្នោតលក់ទេ? មានអ្នកប្រមូលទិញទាំងអស់ហ៊ានឱ្យក្នុងតម្លៃខ្ពស់ ដើម្បីនាំចេញទៅជប៉ុន</a>
                 </h6>
@@ -20,42 +20,42 @@
             <div class="col-xl-8 col-lg-5 col-md-6 col-sm-12 list-komsan2">
                 <div class="row keep-items-4-md">
                     <div class="col-xl-4 col-lg-6 col-md-6 col-sm-4 col-6">
-                        <a href="./images/3.jpg"><img src="./images/3.jpg" alt="" style="width: 100%;"></a>
+                        <a href="./images/3.jpg"><img src="./images/popular/pic1.jpg" alt="" style="width: 100%;"></a>
                         <i class="far fa-calendar-alt"> 2020-01-13</i>
                         <h6 class="list-komsan1-h6">
                             <a class="stretched-link" href="#">មានធាងដូង ​ធាងត្នោតលក់ទេ? មានអ្នកប្រមូលទិញទាំងអស់ហ៊ានឱ្យក្នុងតម្លៃខ្ពស់ ដើម្បីនាំចេញទៅជប៉ុន</a>
                         </h6>
                     </div>
                     <div class="col-xl-4 col-lg-6 col-md-6 col-sm-4 col-6">
-                        <a href="./images/3.jpg"><img src="./images/3.jpg" alt="" style="width: 100%;"></a>
+                        <a href="./images/3.jpg"><img src="./images/popular/pic2.png" alt="" style="width: 100%;"></a>
                         <i class="far fa-calendar-alt"> <b>2020-01-13</b></i>
                         <h6 class="list-komsan1-h6">
                             <a class="stretched-link" href="#">មានធាងដូង ​ធាងត្នោតលក់ទេ? មានអ្នកប្រមូលទិញទាំងអស់ហ៊ានឱ្យក្នុងតម្លៃខ្ពស់ ដើម្បីនាំចេញទៅជប៉ុន</a>
                         </h6>
                     </div>
                     <div class="col-xl-4 col-lg-6 col-md-6 col-sm-4 col-6">
-                        <a href="./images/3.jpg"><img src="./images/3.jpg" alt="" style="width: 100%;"></a>
+                        <a href="./images/3.jpg"><img src="./images/popular/pic3.png" alt="" style="width: 100%;"></a>
                         <i class="far fa-calendar-alt"> <b>2020-01-13</b></i>
                         <h6 class="list-komsan1-h6">
                             <a class="stretched-link" href="#">មានធាងដូង ​ធាងត្នោតលក់ទេ? មានអ្នកប្រមូលទិញទាំងអស់ហ៊ានឱ្យក្នុងតម្លៃខ្ពស់ ដើម្បីនាំចេញទៅជប៉ុន</a>
                         </h6>
                     </div>
                     <div class="col-xl-4 col-lg-6 col-md-6 col-sm-4 col-6">
-                        <a href="./images/3.jpg"><img src="./images/3.jpg" alt="" style="width: 100%;"></a>
+                        <a href="./images/3.jpg"><img src="./images/popular/pic4.jpg" alt="" style="width: 100%;"></a>
                         <i class="far fa-calendar-alt"> <b>2020-01-13</b></i>
                         <h6 class="list-komsan1-h6">
                             <a class="stretched-link" href="#">មានធាងដូង ​ធាងត្នោតលក់ទេ? មានអ្នកប្រមូលទិញទាំងអស់ហ៊ានឱ្យក្នុងតម្លៃខ្ពស់ ដើម្បីនាំចេញទៅជប៉ុន</a>
                         </h6>
                     </div>
                     <div class="col-xl-4 col-lg-6 col-md-6 col-sm-4 col-6">
-                        <a href="./images/3.jpg"><img src="./images/3.jpg" alt="" style="width: 100%;"></a>
+                        <a href="./images/3.jpg"><img src="./images/popular/pic5.png" alt="" style="width: 100%;"></a>
                         <i class="far fa-calendar-alt"> <b>2020-01-13</b></i>
                         <h6 class="list-komsan1-h6">
                             <a class="stretched-link" href="#">មានធាងដូង ​ធាងត្នោតលក់ទេ? មានអ្នកប្រមូលទិញទាំងអស់ហ៊ានឱ្យក្នុងតម្លៃខ្ពស់ ដើម្បីនាំចេញទៅជប៉ុន</a>
                         </h6>
                     </div>
                     <div class="col-xl-4 col-lg-6 col-md-6 col-sm-4 col-6">
-                        <a href="./images/3.jpg"><img src="./images/3.jpg" alt="" style="width: 100%;"></a>
+                        <a href="./images/3.jpg"><img src="./images/popular/pic6.jpg" alt="" style="width: 100%;"></a>
                         <i class="far fa-calendar-alt"> <b>2020-01-13</b></i>
                         <h6 class="list-komsan1-h6">
                             <a class="stretched-link" href="#">មានធាងដូង ​ធាងត្នោតលក់ទេ? មានអ្នកប្រមូលទិញទាំងអស់ហ៊ានឱ្យក្នុងតម្លៃខ្ពស់ ដើម្បីនាំចេញទៅជប៉ុន</a>
